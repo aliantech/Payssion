158bf573f1441fb572aa7ca13f86bac742178a88
aliantech <info@aliantech.com>
2020-03-15 10:39:41 +0800

    Update ConfigProvider.php

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -3,17 +3,17 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\SamplePaymentGateway\Model\Ui;
+namespace Magento\Payssion\Model\Ui;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
-use Magento\SamplePaymentGateway\Gateway\Http\Client\ClientMock;
+use Magento\Payssion\Gateway\Http\Client\ClientMock;
 
 /**
  * Class ConfigProvider
  */
 final class ConfigProvider implements ConfigProviderInterface
 {
-    const CODE = 'sample_gateway';
+    const CODE = 'payssion_gateway';
 
     /**
      * Retrieve assoc array of checkout configuration
